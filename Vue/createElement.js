a3714c5f63d843b92449a33752cdab03f9bdebf9
墨龍 <260669142@qq.com>
2020-01-10 17:09:18 +0800

    Update createElement.js

--- Vue/createElement.js
+++ Vue/createElement.js
@@ -1,5 +1,6 @@
 import { createElement, render, renderDom } from './element.js'
-import diff from './diff'
+import diff from './diff.js'
+import patch from './patch.js'
 
 let vertualDom1 = createElement('ul',{class:'list'},[
   createElement('li',{class:'item'},['a'])
@@ -15,6 +16,8 @@ let vertualDom2 = createElement('ul',{class:'list'},[
 // 将虚拟dom转换为真实dom
 let el = render(vertualDom)
 renderDom(el,window.root)
-
+// 获得补丁包
 let patchs = diff(vertualDom1,vertualDom2)
+// 给元素打补丁 重新更新视图
+patch(el,patches)
 console.log(vertualDom)
