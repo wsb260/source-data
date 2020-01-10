6caef4ff54a421f649b9157c18d714a1f89a3ea9
墨龍 <260669142@qq.com>
2020-01-10 14:40:51 +0800

    将虚拟dom转换为真实dom

--- Vue/createElement.js
+++ Vue/createElement.js
@@ -1,8 +1,11 @@
-import { createElement } from './element.js'
+import { createElement, render, renderDom } from './element.js'
 
 let vertualDom = createElement('ul',{class:'list'},[
   createElement('li',{class:'item'},['a'])
   createElement('li',{class:'item'},['b'])
 ])
+// 将虚拟dom转换为真实dom
+let el = render(vertualDom)
+renderDom(el,window.root)
 
 console.log(vertualDom)
