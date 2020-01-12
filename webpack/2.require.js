c74ad87bdd9b7cfd0714888ec1d574ea757113ab
wsb260 <260669142@qq.com>
2020-01-12 13:16:59 +0800

    t方法分析

--- webpack/2.require.js
+++ webpack/2.require.js
@@ -25,3 +25,31 @@ function n(mod) {
         return mod
     }
 }
+
+// t => 创建一个命名空间对象
+// mode & 1 : value可能是一个模块ID，也可能是一个模块对象,需要加载
+// mode & 8 : 直接返回value，不需要包装成es6.module
+// mode & 4 : 如果value是一ge es6Module,b也就是_esModule=true,不需要包装直接返回
+// mode & 2 : 拷贝一个对象返回
+function t(value,mode) {
+    if(mode & 1) {
+        value = __webpack_require__(value)
+    }
+    if(mode & 8) {
+        return value
+    }
+    if(mode & 4) {
+        if(value.__esModule){
+            return value
+        }
+    }
+    var ns = Object.create(null)
+    ns.__esModule = true
+    ns.default = value
+    if(mode & 2) { // 如果2为true，就把value的所有属性都拷贝到ns对象上
+        for(let key in value) {
+            ns[key] = value[key]
+        }
+    }
+    return ns
+}
